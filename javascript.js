3e036d93035a39d935e355bca77e6f95a19d0915
NexyusNex <shootdietale@gmail.com>
2022-11-29 14:44:57 +0100

    Added an operate function that takes two numbers and an operator and buttons in html

--- javascript.js
+++ javascript.js
@@ -10,3 +10,15 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+function operate(a,b,operator){
+    switch(operator){
+        case"+":console.log(add(a,b)); break;
+        case"-":console.log(substract(a,b)); break;
+        case"/":console.log(divide(a,b)); break;
+        case"*":console.log(multiply(a,b)); break;
+    }
+}
+var a=5;
+var b=2;
+var operator="*";
+operate(a,b,operator);
